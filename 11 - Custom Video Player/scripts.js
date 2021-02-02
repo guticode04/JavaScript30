df3a070a81f10d3f5c6b60572f2d8a9dfe3ceb6d
Gustavo Gutierrez <gusguti04@gmail.com>
2021-02-01 21:17:54 -0800

    Add toglePlay and updateButton functions with event listeners

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,38 @@
+// Get our elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out functions
+
+function togglePlay() {
+   if(video.paused){
+      video.play();
+   } else {
+      video.pause();
+   }
+   // another way to write this logic
+   /*
+      const method = video.paused ? 'play' : 'pause';
+      video[method]();
+   */
+}
+
+function updateButton() {
+   const icon = this.paused ? '►' : '❚ ❚';
+   toggle.textContent = icon;
+
+}
+
+// Hook up the event listners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
