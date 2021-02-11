44b7fa37d60d0bec826bb147ccf0369d8e027314
Gustavo Gutierrez <gusguti04@gmail.com>
2021-02-10 21:28:12 -0800

    Add rgbSplit function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,7 +26,9 @@ function paintToCanvas() {
       // take pixels out
       let pixels = ctx.getImageData(0, 0. width, height);
       // mess with them
-      pixels = redEffect(pixels);
+      // pixels = redEffect(pixels);
+      pixels = rgbSplit(pixels);
+      ctx.globalAlpha = 0.1;
       // put them back
       ctx.putImageData(pixels, 0, 0);
    }, 16);
@@ -51,6 +53,15 @@ function redEffect(pixels) {
       pixels.data[i + 0] = pixels.data[i + 0] + 100 //red
       pixels.data[i + 1] = pixels.data[i + 1] - 50 //green
       pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //blue
+   }
+   return pixels;
+}
+
+function rgbSplit(pixels) {
+   for (let i = 0; i < pixels.data.length; i += 4) {
+      pixels.data[i - 150] = pixels.data[i + 0] + 100 //red
+      pixels.data[i + 500] = pixels.data[i + 1] - 50 //green
+      pixels.data[i - 550] = pixels.data[i + 2] * 0.5 //blue
    }
    return pixels;
 }
