a6b680b9e6b99f95a468448260142a9e50933dfd
Gustavo Gutierrez <gusguti04@gmail.com>
2021-02-23 21:11:46 -0800

    Add displayTimeLeft function

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,15 +1,29 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
    const now = Date.now();
    const then = now + seconds * 1000;
+   displayTimeLeft(seconds);
 
    countdown = setInterval(() => {
       const secondsLeft = Math.round((then - Date.now()) / 1000);
       //check if we should stop it
-      if(seconds <= 0) {
+      if(seconds < 0) {
          clearInterval(countdown);
          return;
       }
+      //display it
+      displayTimeLeft(secondsLeft);
    }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+   const minutes = Math.floor(seconds / 60);
+   const remainderSeconds = seconds % 60;
+   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+   document.title = display;
+   timerDisplay.textContent = display;
+   console.log(minutes, remainderSeconds);
+   
 }
