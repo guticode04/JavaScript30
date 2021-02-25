8ed213a3bc96634e76d6eee3f7955f0ef5805ddd
Gustavo Gutierrez <gusguti04@gmail.com>
2021-02-24 20:55:10 -0800

    Finish day 29 project

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+   // clear any other timer
+   clearInterval(countdown);
+
    const now = Date.now();
    const then = now + seconds * 1000;
    displayTimeLeft(seconds);
@@ -11,7 +15,7 @@ function timer(seconds) {
    countdown = setInterval(() => {
       const secondsLeft = Math.round((then - Date.now()) / 1000);
       //check if we should stop it
-      if(seconds < 0) {
+      if(secondsLeft < 0) {
          clearInterval(countdown);
          return;
       }
@@ -32,8 +36,25 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp) {
    const end = new Date(timestamp);
+   // console.log(end);
    const hour = end.getHours();
+   const adjustedHour = hour > 12 ? hour - 12 : hour;
    const minutes = end.getMinutes();
    endTime.textContent = 
-      `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+      `Be back at ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+   const seconds = parseInt(this.dataset.time);
+   timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function (e) {
+   e.preventDefault();
+   const mins = this.minutes.value;
+   timer(mins * 60);
+   this.reset();
+
+})
