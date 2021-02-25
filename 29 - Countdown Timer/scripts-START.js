fc180a8c3c0743c6b03febffaad92ef12b8eccbf
Gustavo Gutierrez <gusguti04@gmail.com>
2021-02-24 20:26:15 -0800

    Add displayEndTime function to show what time break is over

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
    const now = Date.now();
    const then = now + seconds * 1000;
    displayTimeLeft(seconds);
+   displayEndTime(seconds);
 
    countdown = setInterval(() => {
       const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,6 +26,14 @@ function displayTimeLeft(seconds) {
    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
    document.title = display;
    timerDisplay.textContent = display;
-   console.log(minutes, remainderSeconds);
+   // console.log(minutes, remainderSeconds);
    
+}
+
+function displayEndTime(timestamp) {
+   const end = new Date(timestamp);
+   const hour = end.getHours();
+   const minutes = end.getMinutes();
+   endTime.textContent = 
+      `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
